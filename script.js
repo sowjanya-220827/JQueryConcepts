37022ee614fe3f98030dcc679ef09212e5c7b1fa
sowjanya_220827 <n220827@rguktn.ac.in>
2026-03-16 22:20:39 +0530

    all jquery concepts are completed

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ $("[title*='middle']").css("background","cyan");
 
 $(document).ready(function(){
 
-// 1. Change image source using attr()
+
 
 $("#changeImg").click(function(){
 
@@ -50,7 +50,7 @@ $("#myImage").attr("src","WIN_20250901_16_53_40_Pro.jpg");
 });
 
 
-// 2. Disable checkbox using prop()
+
 
 $("#disableCheck").click(function(){
 
@@ -59,7 +59,7 @@ $("#agree").prop("disabled", true);
 });
 
 
-// Enable checkbox
+
 
 $("#enableCheck").click(function(){
 
@@ -68,7 +68,7 @@ $("#agree").prop("disabled", false);
 });
 
 
-// 3. Retrieve input value using val()
+
 
 $("#showValue").click(function(){
 
@@ -79,7 +79,7 @@ $("#result").text("Entered value: " + name);
 });
 
 
-// 4. removeAttr()
+
 
 $("#remove").click(function(){
 
@@ -172,6 +172,216 @@ alert("Paragraph clicked");
 
 $("#offBtn").click(function(){
 $("#onText").off("click");
+});
+
+});
+
+
+
+
+
+$(document).ready(function(){
+
+// css() method
+$("#cssBtn").click(function(){
+$("#text").css({
+"font-size":"22px",
+"text-decoration":"underline"
+});
+});
+
+
+// addClass()
+$("#addClassBtn").click(function(){
+$("#text").addClass("highlight");
+});
+
+
+// removeClass()
+$("#removeClassBtn").click(function(){
+$("#text").removeClass("highlight");
+});
+
+
+// toggleClass()
+$("#toggleBtn").click(function(){
+$("body").toggleClass("darkTheme");
+});
+
+
+// hasClass()
+$("#checkBtn").click(function(){
+
+if($("#text").hasClass("highlight")){
+$("#result").text("Paragraph has highlight class");
+}
+else{
+$("#result").text("Paragraph does not have highlight class");
+}
+
+});
+
+});
+
+
+$(document).ready(function(){
+
+// Parent Traversal
+
+$("#parentBtn").click(function(){
+
+$(".item").parent().css("border","2px solid black");
+
+});
+
+$("#parentsBtn").click(function(){
+
+$(".item").parents().css("padding","10px");
+
+});
+
+
+// Children Traversal
+
+$("#childrenBtn").click(function(){
+
+$(".parent").children().css("font-weight","bold");
+
+});
+
+
+// Find Method
+
+$("#childrenBtn").click(function(){
+
+$(".grandparent").find("li").css("border","1px dashed black");
+
+});
+
+
+// Sibling Traversal
+
+$("#siblingsBtn").click(function(){
+
+$(".item:first").siblings().css("font-style","italic");
+
+});
+
+
+// Next
+
+$("#nextBtn").click(function(){
+
+$(".item:first").next().css("font-size","20px");
+
+});
+
+
+// NextAll
+
+$("#nextBtn").click(function(){
+
+$(".item:first").nextAll().css("margin","10px");
+
+});
+
+
+// Previous
+
+$("#prevBtn").click(function(){
+
+$(".item:last").prev().css("font-weight","bold");
+
+});
+
+
+// PrevAll
+
+$("#prevBtn").click(function(){
+
+$(".item:last").prevAll().css("padding","5px");
+
+});
+
+
+// Filtering Methods
+
+$("#filterBtn").click(function(){
+
+$("li").first().css("border","2px solid black");
+$("li").last().css("border","2px solid black");
+$("li").eq(2).css("font-size","20px");
+
+$("li").filter(":even").css("font-style","italic");
+
+$("li").not(":first").css("margin","5px");
+
+});
+
+});
+
+
+
+$(document).ready(function(){
+
+// BASIC EFFECTS
+
+$("#showBtn").click(function(){
+$("#box").show();
+});
+
+$("#hideBtn").click(function(){
+$("#box").hide();
+});
+
+$("#toggleBtn").click(function(){
+$("#box").toggle();
+});
+
+
+// FADE EFFECTS
+
+$("#fadeInBtn").click(function(){
+$("#box").fadeIn();
+});
+
+$("#fadeOutBtn").click(function(){
+$("#box").fadeOut();
+});
+
+$("#fadeToggleBtn").click(function(){
+$("#box").fadeToggle();
+});
+
+$("#fadeToBtn").click(function(){
+$("#box").fadeTo("slow",0.5);
+});
+
+
+// SLIDING EFFECTS
+
+$("#slideDownBtn").click(function(){
+$("#box").slideDown();
+});
+
+$("#slideUpBtn").click(function(){
+$("#box").slideUp();
+});
+
+$("#slideToggleBtn").click(function(){
+$("#box").slideToggle();
+});
+
+
+// CUSTOM ANIMATION
+
+$("#animateBtn").click(function(){
+$("#box").animate({
+left:"200px",
+opacity:"0.5",
+height:"200px",
+width:"200px"
+});
 });
 
 });
